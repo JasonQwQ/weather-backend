baffdcaebf57cd804a24bc8ba767614704fb24f4
Jiechen Tan <43553746+JasonQwQ@users.noreply.github.com>
2022-02-06 20:13:07 -0800

    add token requirement (#2)

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/data/2.5/weather', get_weather)
 app.get('/v1/weather', get_weather)
 app.get('/v1/hello', get_hello)
 app.post('/v1/auth',get_auth)
-
+// update token requirement
 function get_weather(request, response) {
     if (request.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c") {
         response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":281.56,"feels_like":281.56,"temp_min":279.46,"temp_max":283.9,"pressure":1032,"humidity":73},"visibility":10000,"wind":{"speed":0.89,"deg":113,"gust":1.34},"clouds":{"all":75},"dt":1642803894,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642779731,"sunset":1642813576},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})    
